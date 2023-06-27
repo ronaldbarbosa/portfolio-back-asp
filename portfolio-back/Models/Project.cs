8f97c1d1b9b1375ff9f5b31eb6c045da47b69e2f
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-06-26 23:16:30 -0300

    Updated Project's fields

--- portfolio-back/Models/Project.cs
+++ portfolio-back/Models/Project.cs
@@ -1,17 +1,20 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace portfolio_back.Models;
 
 public class Project
 {
-    [Column(TypeName = "TEXT")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Frontend { get; set; }
     public string Backend { get; set; }
-    public string DevOps { get; set; }
+    public string Tools { get; set; }
     public string Url { get; set; }
+    public string Code { get; set; }
     public string Image { get; set; }
     public bool Finished { get; set; }
 }
